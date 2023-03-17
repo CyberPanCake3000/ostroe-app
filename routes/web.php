25c7569b1f5ebd8fd9ea6bd4881c5f90e0abf0c7
egatapova <ergatapovaer@gmail.com>
2023-03-17 20:54:18 +0300

    init commit -m

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Auth::routes();
+
+Route::get('/', function(){
+    return redirect()->route('home');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
